8e777fcf17a995debf671452d1157802d7c9f34a
Daniel Higa <danielhiga10@gmail.com>
2023-06-01 15:40:03 -0400

    Set initial page to exams index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :exams
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "exams#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :exams
 end
