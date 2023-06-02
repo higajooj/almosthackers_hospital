d939ebdaa4fe66b3b5b587ab9fe2ee35a306ffda
Daniel Higa <danielhiga10@gmail.com>
2023-06-02 13:24:19 -0400

    Add user helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
 
   has_many :hospital_user_associations
   has_many :hospitals, through: :hospital_user_associations
+
+  def associated_at_hospital?(hospital)
+    hospital.users.find_by(id: id)
+  end
+
+  def role_at_hospital(hospital)
+    hospital_user_associations.find_by(hospital: hospital).role
+  end
 end
