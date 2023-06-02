a4477deb79132953d9fb6d9e14260d666f2e4ddf
Daniel Higa <danielhiga10@gmail.com>
2023-06-02 04:01:10 -0400

    Remove devise recoverable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable, :trackable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable, :trackable
 
   has_many :hospital_user_associations
   has_many :hospitals, through: :hospital_user_associations
