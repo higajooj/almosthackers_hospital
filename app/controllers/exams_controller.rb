7710a4b153503de1867b7562de8f89e4b6bbc38c
Daniel Higa <danielhiga10@gmail.com>
2023-06-02 05:31:10 -0400

    Reformat controllers

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,70 +1,51 @@
 class ExamsController < ApplicationController
-  before_action :set_exam, only: %i[ show edit update destroy ]
+  before_action :set_exam, only: %i[show edit update destroy]
 
-  # GET /exams or /exams.json
   def index
     @exams = Exam.all
   end
 
-  # GET /exams/1 or /exams/1.json
   def show
   end
 
-  # GET /exams/new
   def new
     @exam = Exam.new
   end
 
-  # GET /exams/1/edit
   def edit
   end
 
-  # POST /exams or /exams.json
   def create
     @exam = Exam.new(exam_params)
 
-    respond_to do |format|
-      if @exam.save
-        format.html { redirect_to exam_url(@exam), notice: "Exam was successfully created." }
-        format.json { render :show, status: :created, location: @exam }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @exam.errors, status: :unprocessable_entity }
-      end
+    if @exam.save
+      redirect_to exam_url(@exam), notice: "Exam was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /exams/1 or /exams/1.json
   def update
-    respond_to do |format|
-      if @exam.update(exam_params)
-        format.html { redirect_to exam_url(@exam), notice: "Exam was successfully updated." }
-        format.json { render :show, status: :ok, location: @exam }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @exam.errors, status: :unprocessable_entity }
-      end
+    if @exam.update(exam_params)
+      redirect_to exam_url(@exam), notice: "Exam was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /exams/1 or /exams/1.json
   def destroy
     @exam.destroy
 
-    respond_to do |format|
-      format.html { redirect_to exams_url, notice: "Exam was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to exams_url, notice: "Exam was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_exam
-      @exam = Exam.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def exam_params
-      params.require(:exam).permit(:name, :description)
-    end
+  def set_exam
+    @exam = Exam.find(params[:id])
+  end
+
+  def exam_params
+    params.require(:exam).permit(:name, :description)
+  end
 end
--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,70 +1,51 @@
 class HospitalsController < ApplicationController
-  before_action :set_hospital, only: %i[ show edit update destroy ]
+  before_action :set_hospital, only: %i[show edit update destroy]
 
-  # GET /hospitals or /hospitals.json
   def index
     @hospitals = Hospital.all
   end
 
-  # GET /hospitals/1 or /hospitals/1.json
   def show
   end
 
-  # GET /hospitals/new
   def new
     @hospital = Hospital.new
   end
 
-  # GET /hospitals/1/edit
   def edit
   end
 
-  # POST /hospitals or /hospitals.json
   def create
     @hospital = Hospital.new(hospital_params)
 
-    respond_to do |format|
-      if @hospital.save
-        format.html { redirect_to hospital_url(@hospital), notice: "Hospital was successfully created." }
-        format.json { render :show, status: :created, location: @hospital }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @hospital.errors, status: :unprocessable_entity }
-      end
+    if @hospital.save
+      redirect_to hospital_url(@hospital), notice: "Hospital was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /hospitals/1 or /hospitals/1.json
   def update
-    respond_to do |format|
-      if @hospital.update(hospital_params)
-        format.html { redirect_to hospital_url(@hospital), notice: "Hospital was successfully updated." }
-        format.json { render :show, status: :ok, location: @hospital }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @hospital.errors, status: :unprocessable_entity }
-      end
+    if @hospital.update(hospital_params)
+      redirect_to hospital_url(@hospital), notice: "Hospital was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /hospitals/1 or /hospitals/1.json
   def destroy
     @hospital.destroy
 
-    respond_to do |format|
-      format.html { redirect_to hospitals_url, notice: "Hospital was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to hospitals_url, notice: "Hospital was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_hospital
-      @hospital = Hospital.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def hospital_params
-      params.require(:hospital).permit(:name)
-    end
+  def set_hospital
+    @hospital = Hospital.find(params[:id])
+  end
+
+  def hospital_params
+    params.require(:hospital).permit(:name)
+  end
 end
