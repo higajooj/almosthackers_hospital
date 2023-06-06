cfa01e4f461f6c5890d5b89e1b0c2de9d82531f1
Daniel Higa <danielhiga10@gmail.com>
2023-06-06 02:25:17 -0400

    Add missing association

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,6 +1,7 @@
 class Exam < ApplicationRecord
   belongs_to :pacient, foreign_key: :pacient_id, class_name: "User"
   belongs_to :doctor, foreign_key: :doctor_id, class_name: "User"
+  belongs_to :hospital
 
   validates :name, presence: true
   validates :description, presence: true
